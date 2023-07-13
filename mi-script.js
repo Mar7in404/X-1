28ffc534233e3ea06287432326f2f8dbad19329b
Mar7in <110817533+Mar7in404@users.noreply.github.com>
2023-07-13 09:49:24 -0300

    Add files via upload

---
+++ mi-script.js
@@ -0,0 +1,5 @@
+alert("Hi! This is my CV");
+      document.getElementById("overlay").style.display = "none";
+      function closeAlert() {
+        document.getElementById("overlay").style.display = "none";
+      }
